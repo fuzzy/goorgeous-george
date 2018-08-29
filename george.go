da2fe58818a40ac867c4cba78291d90d1af7c9e3
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2018-08-29 05:16:39 -0700

    started config support, and stubbed static file serving

--- george.go
+++ george.go
@@ -7,14 +7,42 @@ import (
 	"html"
 	"io/ioutil"
 	"net/http"
-	
+
+	"gopkg.in/yaml.v2"
 	"github.com/chaseadamsio/goorgeous"
 )
 
+type Config struct {
+	OrgDir string `yaml:"org_dir"`
+	StaticDir string `yaml:"static_dir"`
+}
+
+func ReadConfig() *Config {
+	retv := &Config{}
+	fn := fmt.Sprintf("%s/george.yml", os.Getenv("PWD"))
+	if _, err := os.Stat(fn); err != nil {
+		log.Fatal(err)
+	}
+	data, err := ioutil.ReadFile(fn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = yaml.Unmarshal([]byte(data), retv)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return retv
+}
+
+func ServeStatic(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "data")
+}
+
 func ServeTheOrgs(w http.ResponseWriter, r *http.Request) {
+	cfg := ReadConfig()
 	if len(html.EscapeString(r.URL.Path)) == 1 {
 		fmt.Fprintf(w, "<html><body><h1>Index</h1><hr /><ul>")
-		fn, er := ioutil.ReadDir("./")
+		fn, er := ioutil.ReadDir(cfg.OrgDir)
 		if er != nil {
 			log.Fatal(er)
 		}
@@ -26,22 +54,25 @@ func ServeTheOrgs(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "</ul></body></html>")
 	} else {
 		fmt.Fprintf(w, "<html><body>")
-		if _, err := os.Stat(fmt.Sprintf(".%s", r.URL.Path)); err == nil {
-			data, der := ioutil.ReadFile(fmt.Sprintf(".%s", r.URL.Path))
+		fname := fmt.Sprintf("%s%s", cfg.OrgDir, r.URL.Path)
+		if _, err := os.Stat(fname); err == nil {
+			data, der := ioutil.ReadFile(fname)
 			if der != nil {
 				log.Fatal(der)
 			}
 			output := goorgeous.OrgCommon([]byte(data))
 			fmt.Fprintf(w, string(output))
-		}
+		} 
 		fmt.Fprintf(w, "</body></html>")
 	}
 }
 
 func main() {
-
-	log.Println("Registering path handlers.")
+	// Register the route functions
 	http.HandleFunc("/", ServeTheOrgs)
+	http.HandleFunc("/static", ServeStatic)
+
+	// Output and start serving
 	log.Println("Serving on 0.0.0.0:8081")
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
