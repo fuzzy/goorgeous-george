0448879fbc8ea2e387d5a826cb49ad2aee81adbb
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2019-05-26 01:15:44 -0700

    this was stupid

--- run.sh
+++ run.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-eval $(ssh-agent -s)
-ssh-add
-ssh git@github.com -T
-echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
-
-export SSH_KNOWN_HOSTS=~/.ssh/known_hosts
 cd /config && /go/bin/goorgeous-george
